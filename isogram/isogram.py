b775a58ff3679b6c3a0bb8e5af8776b49fb09807
Charles Ross <chivalry@mac.com>
2020-04-26 10:54:00 -0700

    finish isogram.py

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,5 +1,7 @@
-from string import ascii_lowercase
-
 def is_isogram(string: str):
-    filtered = [s for s in string.lower() if s in ascii_lowercase]
-    return len(set(filtered)) == len(filtered)
+    chars = set()
+    for char in string.lower():
+        if char.isalpha() and char in chars:
+            return False
+        chars.add(char)
+    return True
