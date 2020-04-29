9b7f7294622ac35a5a8b943abf43965d27318e6d
Charles Ross <chivalry@mac.com>
2020-04-28 20:01:32 -0700

    finish complex_numbers.py

--- complex-numbers/complex_numbers.py
+++ complex-numbers/complex_numbers.py
@@ -1,27 +1,44 @@
+from math import cos, sin, e
+
+
 class ComplexNumber:
     def __init__(self, real, imaginary):
-        pass
+        self.real = real
+        self.imaginary = imaginary
 
     def __eq__(self, other):
-        pass
+        return self.real == other.real and self.imaginary == other.imaginary
 
     def __add__(self, other):
-        pass
+        return ComplexNumber(self.real + other.real, self.imaginary + other.imaginary)
 
     def __mul__(self, other):
-        pass
+        real = self.real * other.real - self.imaginary * other.imaginary
+        imag = self.imaginary * other.real + self.real * other.imaginary
+        return ComplexNumber(real, imag)
 
     def __sub__(self, other):
-        pass
+        return ComplexNumber(self.real - other.real, self.imaginary - other.imaginary)
 
     def __truediv__(self, other):
-        pass
+        numer = self.real * other.real + self.imaginary * other.imaginary
+        denom = other.sum_squares()
+        real = numer / denom
+        numer = self.imaginary * other.real - self.real * other.imaginary
+        imaginary = numer / denom
+        return ComplexNumber(real, imaginary)
 
     def __abs__(self):
-        pass
+        return self.sum_squares() ** 0.5
 
     def conjugate(self):
-        pass
+        return ComplexNumber(self.real, self.imaginary * -1)
 
     def exp(self):
-        pass
+        x = e ** self.real
+        real = cos(self.imaginary)
+        imaginary = sin(self.imaginary)
+        return ComplexNumber(x, 0) * ComplexNumber(real, imaginary)
+
+    def sum_squares(self):
+        return self.real ** 2 + self.imaginary ** 2
