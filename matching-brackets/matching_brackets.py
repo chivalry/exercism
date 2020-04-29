42611cb438a726cc06f8d28033adcb57e86a4bd9
Charles Ross <chivalry@mac.com>
2020-04-28 18:33:39 -0700

    finish matching_brackets.py

--- matching-brackets/matching_brackets.py
+++ matching-brackets/matching_brackets.py
@@ -1,2 +1,15 @@
 def is_paired(input_string):
-    pass
+    if not input_string:
+        return True
+    pairs = {')': '(', ']': '[', '}': '{'}
+    stack = []
+    for c in input_string:
+        if c in pairs.values():
+            stack.append(c)
+        if c in pairs.keys():
+            try:
+                if pairs[c] != stack.pop():
+                    return False
+            except:
+                return False
+    return not bool(stack)
