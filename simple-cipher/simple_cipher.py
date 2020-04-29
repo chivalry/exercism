fd50dae5b60449a43ce66ffab5940822aec4d21a
Charles Ross <chivalry@mac.com>
2020-04-28 18:19:50 -0700

    finish sum_of_multiples

--- simple-cipher/simple_cipher.py
+++ simple-cipher/simple_cipher.py
@@ -1,9 +1,24 @@
+from string import ascii_lowercase
+from random import choice
+
+
 class Cipher:
+    chars = ascii_lowercase
+
     def __init__(self, key=None):
-        pass
+        self.key = key or ''.join([choice(self.chars) for _ in range(100)])
 
     def encode(self, text):
-        pass
+        return self.code(text, 'encode')
 
     def decode(self, text):
-        pass
+        return self.code(text, 'decode')
+
+    def code(self, text, action):
+        sign = 1 if action == 'encode' else -1
+        buf = ''
+        for i, char in enumerate(text):
+            shift = self.chars.find(self.key[i % len(self.key)]) * sign
+            pos = self.chars.find(char)
+            buf += self.chars[(pos + shift) % len(self.chars)]
+        return buf
