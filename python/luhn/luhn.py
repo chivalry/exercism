8d4568e785c12a9b385010ec5b73cee8c736ec1c
Charles Ross <chivalry@mac.com>
2020-06-01 15:19:35 -0700

    finish luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,19 @@
 class Luhn:
     def __init__(self, card_num):
-        pass
+        if not all(c.isdigit() or c == ' ' for c in card_num):
+            self.card = []
+        else:
+            self.card = [int(c) for c in card_num if c.isdigit()]
 
     def valid(self):
-        pass
+        if not self.card or len(self.card) <= 1:
+            return False
+        doubled = []
+        for i, digit in enumerate(reversed(self.card)):
+            if i % 2 == 0:
+                doubled.append(digit)
+            else:
+                cand = digit * 2
+                cand = cand - 9 if cand > 9 else cand
+                doubled.append(cand)
+        return sum(doubled) % 10 == 0
