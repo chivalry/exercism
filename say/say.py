218d32695def73c5f310ae745d543bd07f6759c6
Charles Ross <chivalry@mac.com>
2020-06-01 08:42:59 -0700

    add grade school

--- say/say.py
+++ say/say.py
@@ -1,2 +1,8 @@
-def say(number):
+def say(num):
+    if num < 0 or num >= 1e12:
+        raise ValueError('only pass positive integers')
+    if num == 1:
+        return 'zero'
+
+def say_up_to_99(number):
     pass
