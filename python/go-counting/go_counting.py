e7859df0eaf7fc3b2e16bb7881e8dd2716ba0892
Charles Ross <chivalry@mac.com>
2024-11-14 14:51:47 -0800

    Recomplete go counting

--- python/go-counting/go_counting.py
+++ python/go-counting/go_counting.py
@@ -1,39 +1,46 @@
+from itertools import product
+from collections import defaultdict
+WHITE = 'W'
+BLACK = 'B'
+NONE = ' '
+STONES = {WHITE, BLACK, NONE}
+DELTAS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
 
 class Board:
-    """Count territories of each player in a Go game
-
-    Args:
-        board (list[str]): A two-dimensional Go board
-    """
-
     def __init__(self, board):
-        pass
+        self.rows = board
+        self.columns = list(zip(*board))
+
+    def onboard(self, x, y):
+        return 0 <= x < len(self.columns) and 0 <= y < len(self.rows)
 
     def territory(self, x, y):
-        """Find the owner and the territories given a coordinate on
-           the board
-
-        Args:
-            x (int): Column on the board
-            y (int): Row on the board
-
-        Returns:
-            (str, set): A tuple, the first element being the owner
-                        of that area.  One of "W", "B", "".  The
-                        second being a set of coordinates, representing
-                        the owner's territories.
-        """
-        pass
+        if not self.onboard(x, y):
+            raise ValueError("Invalid coordinate")
+        stack = [(x, y)]
+        territory = set()
+        stones = set()
+        while stack:
+            x, y = stack.pop()
+            if (x, y) not in territory and self.onboard(x, y):
+                stone = self.columns[x][y]
+                if stone == NONE:
+                    territory.add((x, y))
+                    stack += [(x+dx, y+dy) for dx, dy in DELTAS]
+                else:
+                    stones.add(stone)
+        if len(stones) == 1 and len(territory) > 0:
+            return (stones.pop(), territory)
+        else:
+            return (NONE, territory)
 
     def territories(self):
-        """Find the owners and the territories of the whole board
-
-        Args:
-            none
-
-        Returns:
-            dict(str, set): A dictionary whose key being the owner
-                        , i.e. "W", "B", "".  The value being a set
-                        of coordinates owned by the owner.
-        """
-        pass
+        territories = defaultdict(set)
+        for (x, y) in product(range(0, len(self.columns)), range(0, len(self.rows))):
+            stone, territory = self.territory(x, y)
+            territories[stone] |= territory
+        return {
+            stone: territories.get(stone) if territories.get(stone) else set()
+            for stone in STONES
+        }
