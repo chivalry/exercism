55b3a57f7a972de8d061f7955b32c648fc33fc32
Charles Ross <chivalry@mac.com>
2024-11-14 16:43:21 -0800

    complete rotational cipher

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,9 @@
 def rotate(text, key):
-    pass
+    result = ''
+    for char in text:
+        if not char.isalpha():
+            result += char
+            continue
+        diff = 97 if ord(char) >= 97 else 65
+        result += chr((ord(char) - diff + key) % 26 + diff)
+    return result
