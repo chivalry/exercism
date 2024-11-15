620c8ca070d2c05bd40bcb4e2410ad8d23aaf7e9
Charles Ross <chivalry@mac.com>
2024-11-15 10:25:30 -0800

    Finish coaster

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,9 +1,6 @@
 def rotate(text, key):
-    result = ''
-    for char in text:
-        if not char.isalpha():
-            result += char
-            continue
-        diff = 97 if ord(char) >= 97 else 65
-        result += chr((ord(char) - diff + key) % 26 + diff)
-    return result
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    map = alpha[key:] + alpha[:key]
+    translation = str.maketrans((alpha + alpha.upper()),
+                                (map + map.upper()))
+    return text.translate(translation)
