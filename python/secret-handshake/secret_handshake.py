91ce7b99291ab1c6f1d8d9b0f6f8d4ccaeff3bd3
Charles Ross <chivalry@mac.com>
2024-11-16 08:23:54 -0800

    Awaiting review of secret handshake

---
+++ python/secret-handshake/secret_handshake.py
@@ -0,0 +1,27 @@
+ACTIONS = [
+    'wink',
+    'double blink',
+    'close your eyes',
+    'jump'
+]
+
+
+def commands(binary_str):
+    code = int(binary_str, 2)
+    shake = []
+    for i in range(4):
+        if bit_is_set(code, i):
+            shake.append(ACTIONS[i])
+    if bit_is_set(code, 4):
+        shake.reverse()
+    return shake
+
+
+def bit_is_set(num, pos):
+    '''Return `True` if the bit at the given position is on, position moving from left to
+    right starting at 0.
+
+    Works by shifting the bits until the target position is in the 0th spot and bit-wise anding
+    with 1.
+    '''
+    return (num >> pos) & 1
