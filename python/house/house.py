2ff29f425b19262ce3f28ab1ac662987c5647548
Charles Ross <chivalry@mac.com>
2024-11-16 09:36:40 -0800

    mid house projeect

---
+++ python/house/house.py
@@ -0,0 +1,18 @@
+LINES = {
+    'lay in': 'the house that Jack built',
+    'ate': 'the malt',
+    'killed': 'the rat',
+    'worried': 'the cat',
+    'tossed': 'the dog',
+    'milked': 'the cow with the crumpled horn',
+    'kissed': 'the maiden all forlorn',
+    'married': 'the man all tattered and torn',
+    'woke': 'the priest all shaven and shorn',
+    'kept': 'the rooster that crowed in the morn',
+    'belonged to': 'the famer sowing his corn',
+    '': 'the horse and the hound and the horn'
+}
+
+
+def recite(start_verse, end_verse):
+    pass
