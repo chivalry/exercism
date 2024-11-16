a3c58d27517cc4abb2b8f51db427418a0816e878
Charles Ross <chivalry@mac.com>
2024-11-16 07:52:02 -0800

    Awaiting resistor expert and resitor trio review

--- python/resistor-color-expert/resistor_color_expert.py
+++ python/resistor-color-expert/resistor_color_expert.py
@@ -1,2 +1,33 @@
+COLORS = [
+    'black',  'brown',
+    'red',    'orange',
+    'yellow', 'green',
+    'blue',   'violet',
+    'grey',   'white',
+]
+
+TOLERANCES = {
+    'grey': 0.05, 'violet': 0.1,
+    'blue': 0.25, 'green': 0.5,
+    'brown': 1,   'red': 2,
+    'gold': 5,    'silver': 10
+}
+
+
 def resistor_label(colors):
-    pass
+    if len(colors) == 1:
+        return '0 ohms'
+    tolerance = TOLERANCES[colors[-1]]
+    multiplier = 10 ** COLORS.index(colors[-2])
+    base = 10 * COLORS.index(colors[-4]) + COLORS.index(colors[-3])
+    if len(colors) == 5:
+        base += 100 * COLORS.index(colors[0])
+    ohms = base * multiplier
+    prefix = ''
+    for si_pref in ['kilo', 'mega', 'giga']:
+        if ohms < 1000:
+            break
+        ohms /= 1000
+        prefix = si_pref
+    ohms = int(ohms) if ohms == int(ohms) else ohms
+    return f'{ohms} {prefix}ohms ±{tolerance}%'
