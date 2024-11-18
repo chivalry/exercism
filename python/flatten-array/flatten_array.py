c2084392830a35f113d3521de81b4157cde1f63f
Charles Ross <chivalry@mac.com>
2024-11-18 08:11:42 -0800

    Updated flatten based on feedback

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,7 +1,13 @@
+from collections.abc import Iterable
+
+
 def flatten(iterable):
     result = []
     for elem in iterable:
+        '''original code:
         if hasattr(elem, '__iter__'):
+        '''
+        if isinstance(elem, Iterable):
             result.extend(flatten(elem))
         elif elem is not None:
             result.append(elem)
