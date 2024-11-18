64a07a5266a3f5383695311a8ddaacd6dcdc3df2
Charles Ross <chivalry@mac.com>
2024-11-18 08:07:26 -0800

    Update atbash

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -3,15 +3,12 @@ UPPSER = LOWER.upper()
 
 
 def encode(plain_text):
-    plain_text = ''.join(plain_text.split(' '))
     translation = str.maketrans(LOWER + UPPSER, LOWER[::-1] * 2, ' .,')
-    cipher = plain_text.translate(translation)
-    return ' '.join(chunkstring(cipher, 5))
+    return ' '.join(chunkstring(plain_text.replace(' ', '').translate(translation), 5))
 
 
 def decode(ciphered_text):
-    cipher_text = ''.join(ciphered_text.split(' '))
-    return ''.join(encode(cipher_text).split(' '))
+    return encode(ciphered_text.replace(' ', '')).replace(' ', '')
 
 
 def chunkstring(string, length):
