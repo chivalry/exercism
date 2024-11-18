2fee20fb9b32d3cbbe451c8c44c22e001105e6f3
Charles Ross <chivalry@mac.com>
2024-11-18 08:56:53 -0800

    Add suggestion comment

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -4,3 +4,9 @@ def transform(legacy_data):
         for char in legacy_data[key]:
             result[char.lower()] = key
     return result
+
+
+'''mentor comprehension code
+def transform(legacy_data):
+    return {char: score for score, chars in legacy_data.items() for char in map(str.lower, chars)}
+'''
