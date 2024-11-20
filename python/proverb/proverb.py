8760e56eb6d76cde866fd324e44f7a42d4073f36
Charles Ross <chivalry@mac.com>
2024-11-20 14:42:04 -0800

    finish proverb

--- python/proverb/proverb.py
+++ python/proverb/proverb.py
@@ -1,12 +1,13 @@
 def proverb(*args, qualifier=None):
     if not args:
         return []
-    conclusion = f'And all for the want of a {
-        qualifier + ' ' if qualifier else ''}{args[0]}.'
+    # conclusion = f'And all for the want of a {
+    #     qualifier + ' ' if qualifier else ''}{args[0]}.'
+    conclusion = 'And all for the want of a ' + \
+        (qualifier + ' ' if qualifier else '') + args[0] + '.'
     if len(args) == 1:
         return [conclusion]
-    result = ([f'For want of a {word} the {
-        args[idx + 1]} was lost.'
-        for idx, word in enumerate(args[:len(args) - 1])])
+    result = ([f'For want of a {word} the {args[idx + 1]} was lost.'
+               for idx, word in enumerate(args[:len(args) - 1])])
     result.append(conclusion)
     return result
