6ef27dcb7f35930e3d5a9d3633e5c42c0b6d5b03
Charles Ross <chivalry@mac.com>
2024-11-24 11:25:11 -0800

    Update atbash cipher

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,10 +1,14 @@
-LOWER = 'abcdefghijklmnopqrstuvwxyz'
-UPPSER = LOWER.upper()
+import string
+
+LOWER = string.ascii_lowercase
 
 
 def encode(plain_text):
-    translation = str.maketrans(LOWER + UPPSER, LOWER[::-1] * 2, ' .,')
-    return ' '.join(chunkstring(plain_text.replace(' ', '').translate(translation), 5))
+    plain_text = plain_text.lower()
+    translation = str.maketrans(LOWER, LOWER[::-1], ' .,')
+    return ' '.join(chunkstring(
+        plain_text.replace(' ', '').translate(translation), 5)
+    )
 
 
 def decode(ciphered_text):
@@ -12,4 +16,7 @@ def decode(ciphered_text):
 
 
 def chunkstring(string, length):
-    return [string[0 + i:length + i] for i in range(0, len(string), length)]
+    return [
+        string[0 + i:length + i]
+        for i in range(0, len(string), length)
+    ]
--- python/poker/poker.py
+++ python/poker/poker.py
@@ -26,8 +26,9 @@ def best_hands(hands: list[str]) -> list[str]:
         return [hands[0]]
     highest_rank = NO_HIGH_HAND
     high_hands = []
-    for hand in hands:
-        hand = sort(hand)
+    structured_hands = build_hand_map(hands)
+    for structured_hand in structured_hands:
+        hand = structured_hands[structured_hand]
         if is_straight_flush(hand):
             highest_rank = STRAIGHT_FLUSH
             high_hands += [hand]
@@ -55,57 +56,65 @@ def best_hands(hands: list[str]) -> list[str]:
     return [' '.join(high_hand) for high_hand in high_hands]
 
 
+def build_hand_map(hands: list[str]) -> dict[str, list[str]]:
+    """Build a structure where the text hand is the key and the sorted hand is the value
+    :param hands: list[str] - The hands to build the structure from
+    :return dict[str, list[str]] - The structure to return
+    """
+    return {hand: sort(hand) for hand in hands}
+
+
 def sort(hand: str) -> list[str]:
-    '''Return a sorted version of the hand as a list of cardds
+    """Return a sorted version of the hand as a list of cardds
     :param hand: str - The hand to sort as a string of cards
     :return list[str] - The sorted hand, high card to low, as a list of card strings
-    '''
+    """
     return sorted(hand.split(' '), key=card_rank)
 
 
 def card_rank(card: str) -> int:
-    '''Return the rank of the given card
+    """Return the rank of the given card
     :param card: str - A string representing the card
     :return int - The rank of the card, lower being better
-    '''
+    """
     return CARD_RANKS.index('10' if card[0] == '1' else card[0])
 
 
 def values(hand: list[str]) -> set[int]:
-    '''Return the unique values of the hand
+    """Return the unique values of the hand
     :param hand: list[str] - The hand to evaluate
     :return set[int] - The unique values
-    '''
+    """
     return {card_rank(card) for card in hand}
 
 
 def value_count(hand: list[str]) -> int:
-    '''Return the number of card values in the hand
+    """Return the number of card values in the hand
     :param card: str = A string representing the card
     :return int - The number of unique card values found in the hand
-    '''
+    """
     return len(values(hand))
 
 
 def is_straight_flush(hand: list[str]) -> bool:
-    '''Return `True` if the hand is a straight fluch (all same suit with sequential
+    """Return `True` if the hand is a straight fluch (all same suit with sequential
     values)
     :param hand: list[str] - The hand to evaluate
     :return bool - Whether the hand is a straight flush
-    '''
+    """
     if not all(card[-1] == hand[0][-1] for card in hand):
         return False
     return is_straight(hand)
 
 
 def is_four_of_a_kind(hand: list[str]) -> bool:
-    '''Return `True` if the hand is four of a lind (at least four of the same suit)
+    """Return `True` if the hand is four of a lind (at least four of the same suit)
     :param hand: list[str] - The hand to evaluate
     :return bool - Whether the hand is four of a kind
     :note - Takes advantage of the hand being sorted by value, which implies, if there are only
             two unique values, either the first one differs from the second or the last differs
             from the second-to-last
-    '''
+    """
     if value_count(hand) != 2:
         return False
     return card_rank(hand[0]) != card_rank(hand[1]) \
@@ -113,13 +122,13 @@ def is_four_of_a_kind(hand: list[str]) -> bool:
 
 
 def is_full_house(hand: list[str]) -> bool:
-    '''Return `True` if the hand is a full house (two of one value, three of another)
+    """Return `True` if the hand is a full house (two of one value, three of another)
     :param hand: list[str] - The hand to evaluate
     :return bool - Whether the hand is a full hours
     :note - Takes advantage of the hand being sorted by value, which implies, if there are only
             two unique values, either the second card differs from the third or the third card
             differs from the fourth
-    '''
+    """
     if value_count(hand) != 2:
         return False
     return card_rank(hand[1]) != card_rank(hand[2]) \
@@ -127,19 +136,19 @@ def is_full_house(hand: list[str]) -> bool:
 
 
 def is_flush(hand: list[str]) -> bool:
-    '''Return `True` if the hand is a flush (all same suit)
+    """Return `True` if the hand is a flush (all same suit)
     :param hand: list[str] - The hand to evaluate
     :return bool - Whether the hand is a flush
     :note - Checks that the suite of all the cards matches the suit of the first card
-    '''
+    """
     return all([hand[0][-1] == card[-1] for card in hand])
 
 
 def is_straight(hand: list[str]) -> bool:
-    '''Return `True` if the hand is a straight (sequential values)
+    """Return `True` if the hand is a straight (sequential values)
     :param hand: list[str] - The hand to evaluate
     :return bool - Whether the hand is a straight
-    '''
+    """
     rank = card_rank(hand[0])
     for card in hand[1:]:
         curr_rank = card_rank(card)
@@ -150,32 +159,32 @@ def is_straight(hand: list[str]) -> bool:
 
 
 def is_three_of_a_kind(hand: list[str]) -> bool:
-    '''Return `True` if the hand is a three-of-a-kind (three of one value, two others not of
+    """Return `True` if the hand is a three-of-a-kind (three of one value, two others not of
     same value)
     :param hand: list[str] - The hand to evaluate
     :return bool - Whether the hand is a straight
-    '''
+    """
     if value_count(hand) != 3:
         return False
 
 
 def is_two_pair(hand: list[str]) -> bool:
-    '''Return `True` if the hand is two pair (two of one value, two of another value, last of
+    """Return `True` if the hand is two pair (two of one value, two of another value, last of
     different value from pairs)
     :param hand: list[str] - The hand to evaluate
     :return bool - Whether the hand is a straight
-    '''
+    """
     if value_count(hand) != 3:
         return False
 
 
 def is_one_pair(hand: list[str]) -> bool:
-    '''Return `True` if the hand is a pair (two of one value, three others not of duplicated
+    """Return `True` if the hand is a pair (two of one value, three others not of duplicated
     values)
     :param hand: list[str] - The hand to evaluate
     :return bool - Whether the hand is a straight
-    '''
+    """
     pass
 
 
-print(best_hands(["4D 5D 6D 7D 8D", "2S 4S 3S 5S 6S", "10S 4S 5D 6H JH"]))
+print(best_hands(["4D 5D 6D 7D 8D", "9D 5D 6D 7D 8D"]))
